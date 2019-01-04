e1a36f3fba43de1297ea22f7ad1059b69b6083f5
superstar360 <abelsobmenso@gmail.com>
2019-01-05 01:27:27 +0300

    new feature modification

---
+++ Resources/js/scroller.js
@@ -0,0 +1,18 @@
+let scroll = document.getElementById("scroll");
+let nav    = document.getElementById("navid");
+let span   = document.getElementById("span-scroll")
+
+window.addEventListener("scroll", () => {
+    
+    const scrolled = window.scrollY;
+    if (scrolled > 0) {
+        scroll.classList.add("backgrd-clr");
+        nav.classList.add("scroll-nav");
+        span.classList.add("scroll-nav");
+    } else {
+        scroll.classList.remove("backgrd-clr");
+        nav.classList.remove("scroll-nav");
+        span.classList.remove("scroll-nav");
+    }
+    
+});
